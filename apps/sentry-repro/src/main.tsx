bd65ef3cbc8c4d6857b9123bc9c911dc60462386
Julian Paas <julian.paas@osaaru.com>
2023-02-07 16:39:53 -0500

    remove react-native-config

--- apps/sentry-repro/src/main.tsx
+++ apps/sentry-repro/src/main.tsx
@@ -2,16 +2,19 @@ import * as Sentry from "@sentry/react-native"
 import { AppRegistry } from 'react-native';
 import App from './app/App';
 
+const dsn = process.env.NX_SENTRY_DSN
+const dist = process.env.NX_SENTRY_DIST || "1"
+
 Sentry.init({
   debug: true,
-  dist: process.env.NX_SENTRY_DIST || "1",
-  dsn: process.env.NX_SENTRY_DSN,
+  dist,
+  dsn,
   environment: "development",
   release: "1.0.0",
   tracesSampleRate: 1.0
 })
 
-console.info("sentry-repro starting...", process.env)
+console.info("sentry-repro starting...", { dist, dsn })
 const usingHermes = typeof HermesInternal === "object" && HermesInternal !== null
 console.info(`Hermes js engine: ${usingHermes}`)
 
